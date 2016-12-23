13e558f487475282af1b8f4588a277132c57224c
Alex Ballera <alexballera@gmail.com>
2016-12-22 20:46:25 -0400

    Configurado babelrc

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,7 +3,7 @@ require('require-dir')('./tasks')
 
 /* Instalar las siguientes dependencias:
 
-sudo npm i -D babel-preset-latest babelify browser-sync browserify del gulp gulp-autoprefixer gulp-cache gulp-cssnano gulp-gh-pages gulp-htmlmin gulp-imagemin gulp-inject gulp-notify gulp-plumber gulp-rename gulp-sass gulp-sass-glob gulp-uglify gulp-uncss imagemin-jpegtran imagemin-optipng imagemin-pngquant imagemin-svgo jquery require-dir vinyl-buffer vinyl-source-stream
+sudo npm i -D gulp babel-preset-latest babelify browser-sync browserify del gulp-autoprefixer gulp-cache gulp-cssnano gulp-gh-pages gulp-htmlmin gulp-imagemin gulp-inject gulp-notify gulp-plumber gulp-rename gulp-sass gulp-sass-glob gulp-uglify gulp-uncss imagemin-jpegtran imagemin-optipng imagemin-pngquant imagemin-svgo jquery require-dir vinyl-buffer vinyl-source-stream
 
 */
 // Build
